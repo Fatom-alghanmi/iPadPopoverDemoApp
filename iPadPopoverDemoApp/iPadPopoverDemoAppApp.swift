6ef055742d8594165055196a270b94dbeebbfd0e
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-10-21 08:17:29 -0400

    Initial Commit

---
+++ iPadPopoverDemoApp/iPadPopoverDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadPopoverDemoAppApp.swift
+//  iPadPopoverDemoApp
+//
+//  Created by Fatom on 2025-10-21.
+//
+
+import SwiftUI
+
+@main
+struct iPadPopoverDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iPadPopoverDemoAppTests/iPadPopoverDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iPadPopoverDemoAppTests.swift
+//  iPadPopoverDemoAppTests
+//
+//  Created by Fatom on 2025-10-21.
+//
+
+import Testing
+@testable import iPadPopoverDemoApp
+
+struct iPadPopoverDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
